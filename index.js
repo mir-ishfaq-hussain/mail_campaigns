2effc9bbb2e6e559c29425013ba9518fadfd7570
ishfaq hussain <mirishfaqhussain007@gmail.com>
2024-10-18 15:06:49 +0530

    completed the oAuth

--- index.js
+++ index.js
@@ -8,13 +8,18 @@ passport.use(new GoogleStrategy({
     clientID:keys.googleClientId,
     clientSecret:keys.googleSecretKey,
     callbackURL:'/auth/google/callback'
-},(accessToken)=>{
-    console.log(accessToken)
+},(accessToken,refreshToken,profile,done)=>{
+    console.log('accessToken',accessToken)
+    console.log('refreshToken',refreshToken)
+    console.log('profile',profile)
+    
 }))
 
 app.get('/auth/google',passport.authenticate('google',{
     scope:['profile','email']
 }))
+
+app.get('/auth/google/callback',passport.authenticate('google'))
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT)
