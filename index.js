a10b79804e8ca091343f3f1ca5a3c359fab1ee9a
ishfaq hussain <mirishfaqhussain007@gmail.com>
2024-10-18 12:15:21 +0530

    added the strategy

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 const express = require('express')
 const app =express()
-app.get('/',(req,res)=>{
-    res.send({hi:"theres"})
+const passport = require('passport')
+const GoogleStrategy = require('passport-google-oauth20').Strategy
+const keys = require('./config/keys.js')
 
-})
+passport.use(new GoogleStrategy({
+    clientID:keys.googleClientId,
+    clientSecret:keys.googleSecretKey,
+    callbackURL:'/auth/google/callback'
+},(accessToken)=>{
+    console.log(accessToken)
+}))
+
+app.get('/auth/google',passport.authenticate('google',{
+    scope:['profile','email']
+}))
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT)
