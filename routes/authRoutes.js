6555fa8947788dd30acf44fa30efbf9b4882c3a2
ishfaq hussain <mirishfaqhussain007@gmail.com>
2024-10-23 14:05:13 +0530

    added the full authentication flow

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -6,4 +6,13 @@ module.exports = app=>{
     }))
     
     app.get('/auth/google/callback',passport.authenticate('google'))
+
+    app.get('/api/logout',(req,res)=>{
+        req.logout() //this function just destroys the req.user object and its provided by the passport library
+        res.send(req.user)
+    })
+
+    app.get('/api/current_user',(req,res)=>{
+        res.send(req.user)
+    })
 }
